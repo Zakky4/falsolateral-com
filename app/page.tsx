7f24872177152e4c9165c1b0b9af0b98dce3e014
zakky <sadaoza@gmail.com>
2024-07-27 16:28:14 +0900

    Edit Globals & Scoped page Style

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>プログミングで業務効率化</h1>
-      <p>私たちはコードで業務を改善する会社です。</p>
+      <h1 className={styles.title}>プログミング x 業務改善</h1>
+      <p className={styles.description}>私たちはヘルプデスクを中心とした業務を効率化する会社です。</p>
     </div>
   );
 }
