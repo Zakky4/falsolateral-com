b3605cadd053af406bfea2005a3608e6c31ae47d
zakky <sadaoza@gmail.com>
2024-07-27 16:38:19 +0900

    Add Top Image

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,22 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>プログミング x 業務改善</h1>
-      <p className={styles.description}>私たちはヘルプデスクを中心とした業務を効率化する会社です。</p>
-    </div>
+    <section className="{style.top}">
+      <div>
+        <h1 className={styles.title}>プログミング x 業務改善</h1>
+        <p className={styles.description}>
+          私たちはヘルプデスクを中心とした業務を効率化する会社です。
+        </p>
+      </div>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+        priority
+        sizes="100vw"
+      />
+    </section>
   );
 }
