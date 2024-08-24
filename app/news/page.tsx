d526b7359b32b5e486ae6746ed32d2c862e4e795
zakky <sadaoza@gmail.com>
2024-08-24 16:39:46 +0900

    Add Pagination

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,6 @@
 import { getNewsList } from '@/app/_libs/microcms';
 import NewsList from '@/app/_components/NewsList';
-// import Pagination from '@/app/_components/Pagination';
+import Pagination from '@/app/_components/Pagination';
 // import SearchField from '@/app/_components/SearchField';
 import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
@@ -13,7 +13,7 @@ export default async function Page() {
     <>
       {/* <SearchField /> */}
       <NewsList news={news} />
-      {/* <Pagination totalCount={totalCount} /> */}
+      <Pagination totalCount={totalCount} />
     </>
   );
 }
