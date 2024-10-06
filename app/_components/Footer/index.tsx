081cba528f81c22da4ed9619e8dc81f78261d31f
zakky <sadaoza@gmail.com>
2024-10-06 21:42:11 +0900

    Change All Rights Reserved

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
           </li> */}
         </ul>
       </nav>
-      <p className={styles.cr}>© SIMPLE. All Rights Reserved 2024</p>
+      <p className={styles.cr}>© falsolateral. All Rights Reserved</p>
     </footer>
   );
 }
